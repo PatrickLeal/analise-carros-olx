cef9db60734e2ef71fb63a3ce66a3de11c62596f
Patrick Leal <pbsleal@hotmail.com>
2024-09-19 20:21:38 -0300

    fix: consertando funcao limpar_dados_silver

--- scripts/cleaning_tool.py
+++ scripts/cleaning_tool.py
@@ -10,15 +10,17 @@ def limpar_dados_silver(data: DataFrame) -> DataFrame:
     df = df.drop_duplicates()
     df = df.drop(columns=['TIPO_ANUNCIO', 'FINAL_DE_PLACA', 'CATEGORIA']).reset_index(drop=True)
 
-    df.OPCIONAIS = df.OPCIONAIS.apply(lambda x: ast.literal_eval(x))
+    df.OPCIONAIS = df.OPCIONAIS.str.replace('[', '').str.replace(']', '').str.replace("'", "")
     idx_list = [idx for idx, linha in enumerate(df['OPCIONAIS']) if len(linha) < 1]     
     df = df.drop(index=idx_list) # removendo os registros que não tem 'opcionais'
-    df['qtd_opcionais'] = [len(opc) for opc in df.OPCIONAIS]
+    df['qtd_opcionais'] = [len(opc.split(',')) for opc in df.OPCIONAIS]
 
     df = df.dropna().reset_index(drop=True)
     df.ANO = df.ANO.str.replace('ou anterior', '').str.strip().astype(int)
     df.PORTAS = df.PORTAS.str.replace('portas', '').str.strip().astype(int)
     df.POTENCIA_DO_MOTOR = df.POTENCIA_DO_MOTOR.astype('category')
+    df.CEP = df.CEP.astype('category')
+    df.PRECO_BRL = df.PRECO_BRL.astype(int)
 
     df.columns = df.columns.str.lower()
 
